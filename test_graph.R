8c7091404f56da96c7e5abb7598a5fd9bbdb9666
habdulkafi <habdulkafi@wesleyan.edu>
2017-04-28 00:49:16 -0400

    added more charts and data

--- test_graph.R
+++ test_graph.R
@@ -4,6 +4,10 @@
 library(igraph)
 library(tidyverse)
 library(scales)
+library(ggplot2)
+
+library(ggthemes)
+theme_set(theme_fivethirtyeight())
 
 
 data <- read_csv("top_t_d_2000_500_2.csv")
@@ -23,3 +27,21 @@ plot( grph, layout=layout_as_bipartite, vertex.label=NA, vertex.size=2 )
 
 bgrph <- bipartite.projection(grph)
 plot( grph, layout=layout.auto, vertex.label=NA, vertex.size=2 )
+
+
+day_data <- read_csv("t_d_n_posts_oct_nov_dec.csv")
+
+
+day_data %>% ggplot(aes(day_of_month, n_posts)) + geom_col()
+
+
+n_gilds <- read_csv("n_gilds_jan_feb_mar.csv")
+
+
+n_gilds %>% ggplot(aes(month,n_gilds)) + 
+  geom_bar(aes(fill=domain), position = "dodge", stat="identity")
+
+
+
+
+
