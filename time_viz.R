1a597cff6238f04d5996e667aa4d6421f1b68428
dragoncsc <sd2920@columbia.edu>
2017-05-05 13:33:25 -0400

    added some pretty graphs

--- time_viz.R
+++ time_viz.R
@@ -5,9 +5,12 @@ library(scales)
 library(dplyr)
 
 
-data1 <- read_csv("b4_algo1_500_100_2.csv")
-data2 <- read_csv("after_algo1_500_100_2.csv")
-#data <- data %>% filter(n_links > 1)
+#data1 <- filter(read_csv("data/b4_algo1_translated.csv"), n_links > 1)
+#data2 <- filter(read_csv("data/after_algo1_translated.csv"), n_links >  1)
+data1 <- filter(read_csv("data/b4_inaug_translated.csv"), n_links > 1)
+data2 <- filter(read_csv("data/after_inaug_translated.csv"), n_links >  1)
+
+
 grph1 <- graph.data.frame(data1, directed = F)
 grph2 <- graph.data.frame(data2, directed = F)
 
@@ -17,7 +20,7 @@ V(grph2)$label <- names(V(grph2))
 #plot(grph, layout = layout.auto, vertex.label=ifelse(degree(grph )>30,V(grph)$label, NA), vertex.size=2)
 
 
-defaults <- c("Art","AskReddit","askscience","aww","books","creepy",
+defaults <- c("politics", "The_Donald", "Art","AskReddit","askscience","aww","books","creepy",
               "dataisbeautiful","Documentaries","EarthPorn","explainlikeimfive","food",
               "funny","Futurology","gadgets","gaming","GetMotivated","gifs","history","IAmA",
               "InternetIsBeautiful","Jokes","LifeProTips","listentothis","mildlyinteresting",
@@ -28,8 +31,8 @@ defaults <- c("Art","AskReddit","askscience","aww","books","creepy",
 actual_defaults1 <- names(V(grph1))[names(V(grph1)) %in% defaults]
 actual_defaults2 <- names(V(grph2))[names(V(grph2)) %in% defaults]
 
-grph1 <- delete_vertices(grph1, actual_defaults)
-grph2 <- delete_vertices(grph2, actual_defaults)
+grph1 <- delete_vertices(grph1, actual_defaults1)
+grph2 <- delete_vertices(grph2, actual_defaults2)
 
 #plot(grph2, layout = layout.auto, vertex.label=ifelse(degree(grph2 )>15,V(grph2)$label, NA), vertex.size=2)
 
@@ -42,9 +45,13 @@ grph2 <- delete_vertices(grph2, which(degree(grph2) <= 1))
 #grph4 <- delete_vertices(grph4, which(degree(grph4) <= 2))
 
 par(mfrow=c(1,2))
-plot(grph1, layout=layout.auto,  vertex.label=ifelse(degree(grph4 )>5,V(grph4)$label, NA), vertex.size=2)
-plot(grph2, layout=layout.auto,  vertex.label=ifelse(degree(grph4 )>5,V(grph4)$label, NA), vertex.size=2)
-
+plot(grph1, layout=layout.auto, vertex.label=ifelse(degree(grph1 )>1,
+                                                    V(grph2)$label, NA), vertex.size=2)
+plot(grph2, layout=layout.auto, vertex.label=ifelse(degree(grph2 )>1,
+                                                    V(grph2)$label, NA), vertex.size=2)
+mtext("The_Donald users subreddit preferences before and after inauguration",
+      sub="Labels added to subreddits with degree greater than 5",
+      side = 3, line = -5, outer = TRUE)
 
 
 
